d492478a9a51354373068521d47c3d467dfbcd75
merma158 <guason158@hotmail.com>
2013-07-22 21:58:28 -0430

    Create SocketControl.h

---
+++ SocketControl.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   SocketControl.h
+ * Author: merma
+ *
+ * Created on July 19, 2013, 12:51 AM
+ */
+
+#ifndef SOCKETCONTROL_H
+#define  SOCKETCONTROL_H
+
+#define PORT 9513
+
+using namespace std;
+
+class SocketControl {
+public:
+    SocketControl();
+    SocketControl(const SocketControl& orig);
+    virtual ~SocketControl();
+    bool open();
+    bool conectar();
+    string leer();
+    bool escribir(string s);
+    void desconectar();
+    void cerrar();
+private:
+    int sockfd,newsockfd,n;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+
+};
+
+#endif	/* SOCKETCONTROL_H */
+
