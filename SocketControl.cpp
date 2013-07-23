e5d7eccdf29df3aad7dbb8093467d924b17bc2a6
merma158 <guason158@hotmail.com>
2013-07-22 21:58:55 -0430

    Create SocketControl.cpp

---
+++ SocketControl.cpp
@@ -0,0 +1,78 @@
+/* 
+ * File:   SocketControl.cpp
+ * Author: merma
+ * 
+ * Created on July 19, 2013, 12:51 AM
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "SocketControl.h"
+
+using namespace std;
+
+SocketControl::SocketControl() {}
+
+SocketControl::SocketControl(const SocketControl& orig) {}
+
+SocketControl::~SocketControl() {}
+
+bool SocketControl::open(){
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+         return false;
+     
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(PORT);
+     return true;
+}
+
+bool SocketControl::conectar(){
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+  return false;
+    return true;
+}
+
+string SocketControl::leer(){
+    string salida;
+    listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     
+     if (newsockfd < 0)          
+         return "ERROR ACCEPT"; 
+     
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) 
+         return "ERROR LEER";
+     
+     salida.append(buffer);
+     return salida;
+}
+
+bool SocketControl::escribir(string s){        
+    const char *cc = s.c_str();
+    strcpy(buffer,cc);
+    n = write(newsockfd,buffer,s.length());
+    if (n < 0)
+        return false;
+    return true;
+}
+
+void SocketControl::desconectar(){
+    close(newsockfd);
+}
+
+void SocketControl::cerrar(){    
+    close(sockfd);
+}
